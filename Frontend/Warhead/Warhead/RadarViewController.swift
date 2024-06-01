dfa8cf2fd21f1d293317cc51de548b2221f57f30
Alok Sahay <alok.sahay87@gmail.com>
2024-06-01 18:32:59 +0200

    Added warheads on mapview

--- Frontend/Warhead/Warhead/RadarViewController.swift
+++ Frontend/Warhead/Warhead/RadarViewController.swift
@@ -10,10 +10,12 @@ import CoreLocation
 import MapKit
 
 class RadarViewController: UIViewController  {
-
+    
     let locationManager = CLLocationManager()
-    var mapView: MKMapView = MKMapView()
+    @IBOutlet weak var mapView: MKMapView!
     var lastKnownLocation = CLLocation()
+    var overlays = [MKOverlay]()
+    let testCoordinate = CLLocationCoordinate2D(latitude: 50.09389700161148, longitude: 14.450689047959024)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,12 +23,66 @@ class RadarViewController: UIViewController  {
         locationManager.delegate = self
         locationManager.requestAlwaysAuthorization()
         
-        mapView.mapType = .standard
+        mapView.mapType = .hybridFlyover
         mapView.showsUserLocation = true
         mapView.userTrackingMode = .follow
+        mapView.delegate = self
+        let span = MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.005) // Adjust the zoom level
+        let region = MKCoordinateRegion(center: lastKnownLocation.coordinate, span: span)
         
+        mapView.setRegion(region, animated: true)
     }
 }
+
+extension RadarViewController: MKMapViewDelegate {
+    
+    func addImpactArea(at coordinate: CLLocationCoordinate2D, radius: CLLocationDistance) {
+        let circle = MKCircle(center: coordinate, radius: radius)
+        circle.title = "impact"
+        mapView.addOverlay(circle)
+        overlays.append(circle)
+    }
+    
+    func addAirdropArea(at coordinate: CLLocationCoordinate2D, radius: CLLocationDistance) {
+        let circle = MKCircle(center: coordinate, radius: radius)
+        circle.title = "airdrop"
+        mapView.addOverlay(circle)
+        overlays.append(circle)
+    }
+    
+    
+    func removeOverlay(_ overlay: MKOverlay) {
+        mapView.removeOverlay(overlay)
+        overlays.removeAll { $0 === overlay }
+    }
+    
+    func removeAllOverlays() {
+        mapView.removeOverlays(mapView.overlays)
+        overlays.removeAll()
+    }
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        if let circleOverlay = overlay as? MKCircle {
+            let circleRenderer = MKCircleRenderer(circle: circleOverlay)
+            
+            switch circleOverlay.title {
+            case "impact":
+                circleRenderer.fillColor = UIColor.red.withAlphaComponent(0.2)
+                circleRenderer.strokeColor = UIColor.red
+                circleRenderer.strokeColor = UIColor.red
+            case "airdrop":
+                circleRenderer.fillColor = UIColor.green.withAlphaComponent(0.2)
+                circleRenderer.strokeColor = UIColor.green
+                circleRenderer.strokeColor = UIColor.green
+            default: break
+            }
+            circleRenderer.lineWidth = 2
+            return circleRenderer
+        }
+        return MKOverlayRenderer(overlay: overlay)
+    }
+}
+
 
 extension RadarViewController: CLLocationManagerDelegate {
     
@@ -35,11 +91,12 @@ extension RadarViewController: CLLocationManagerDelegate {
             locationManager.startUpdatingLocation()
         }
     }
-
+    
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         if let location = locations.last, location.distance(from: lastKnownLocation) > 100 {
             print("Current location: \(location)")
             lastKnownLocation = location
+            addAirdropArea(at: testCoordinate, radius: 200)            
         }
     }
 }
